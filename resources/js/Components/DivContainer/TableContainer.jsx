e90de8d91a6fe26abc542a6ef7516f633eeb6ac8
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-26 15:18:47 +0800

    * added table container

---
+++ resources/js/Components/DivContainer/TableContainer.jsx
@@ -0,0 +1,22 @@
+export default function TableContainer({ children, tableIcon, tableTitle, borderColor, headerContent }) {
+    return (
+        <>
+            <div className="w-full flex flex-col items-center justify-between sm:flex-row">
+                <span className="flex items-center gap-1 ">
+                    <p className="ml-3 mb-4 text-3xl font-semibold">
+                        {tableIcon}
+                    </p>
+                    <p className="ml-3 mb-4 text-3xl font-semibold">
+                        {tableTitle}
+                    </p>
+                </span>
+                {headerContent}
+            </div>
+            <div className={`overflow-hidden bg-gray-100 shadow-lg sm:rounded-lg dark:bg-black border-l-8 ${borderColor}`}>
+                <div className="p-1 border-t border-b border-r border-gray-300 dark:border-stone-800 rounded-r-lg">
+                    {children}
+                </div>
+            </div>
+        </>
+    )
+}
