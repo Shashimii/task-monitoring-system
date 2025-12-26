237c34140d89fd6a086db7ae1ede56b490deb86d
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-26 15:19:51 +0800

    * added recent tasks monitoring up to 5 rows

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -217,6 +217,76 @@ export default function Dashboard({ task_counts = {}, recent_tasks = [], tasks_b
                         </div>
                     </TableContainer>
                 </div>
+
+                <div className="space-y-4">
+                    <div className="flex items-center space-x-4">
+                        <h1 className="text-3xl">🧐</h1>
+                        <h1 className="text-3xl font-semibold">Recent Tasks</h1>
+                    </div>
+                    {recent_tasks.length > 0 ? (
+                        recent_tasks.map(task => (
+                            <div key={task.id} className="bg-white p-4 border border-gray-300 rounded-lg shadow-lg dark:bg-black dark:border-stone-800">
+                                <div className="flex items-center space-x-4">
+                                    <div className="px-4 py-4 bg-green-200 rounded-md text-green-800">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                        </svg>
+                                    </div>
+                                    <div className="w-full space-y-2">
+                                        <div className="flex items-center justify-between">
+                                            <div className="flex space-x-1">
+                                                <p className="text-sm font-semibold">Task Created at</p>
+                                                <p className="text-sm font-semibold">{task?.created_at}</p>
+                                            </div>
+                                            <div className="flex space-x-4">
+                                                <div className="px-4 py-1 rounded" style={{ backgroundColor: task.division?.division_color || '#gray' }}>
+                                                    <p className="text-sm">{task.division?.division_name}</p>
+                                                </div>
+                                                <div className={`flex items-center px-4 rounded-full
+                                                            ${task?.status === "Completed" && "bg-green-300 dark:bg-green-800"}
+                                                            ${task?.status === "In Progress" && "bg-orange-300 dark:bg-orange-600"}
+                                                            ${task?.status === "Not Started" && "bg-gray-300 dark:bg-gray-800"}
+                                                        `}>
+                                                    <p className="text-sm">{task?.status}</p>
+                                                </div>
+                                                {task?.due_date && (
+                                                    <div className="px-4 bg-red-200 text-red-600 font-semibold flex items-center rounded-full">
+                                                        <p className="text-sm">{task?.due_date}</p>
+                                                    </div>
+                                                )}
+                                                {task?.priority && (
+                                                    <div className={`px-4 flex items-center rounded
+                                                            ${task?.priority === "Low" && "bg-green-300 dark:bg-green-800"}
+                                                            ${task?.priority === "Medium" && "bg-orange-300 dark:bg-orange-600"}
+                                                            ${task?.priority === "High" && "bg-red-300 dark:bg-red-700"}
+                                                        `}>
+                                                        <p className="text-sm">{task?.priority}</p>
+                                                    </div>
+                                                )}
+                                            </div>
+                                        </div>
+                                        <div className="">
+                                            <p className="text-2xl">{task?.name}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    ) : (
+                        <div className="bg-white p-4 border border-gray-300 rounded-lg shadow-lg dark:bg-black dark:border-stone-800">
+                            <div className="flex items-center space-x-4">
+                                <div className="px-4 py-4 bg-blue-200 rounded-md text-blue-800">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                                    </svg>
+                                </div>
+                                <div className="pr-10 w-full flex justify-center">
+                                    <h1 className="text-2xl font-semibold">No new tasks have been created at this time</h1>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
             </MainContainer>
         </AuthenticatedLayout>
     );
