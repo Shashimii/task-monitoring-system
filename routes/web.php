850fef84f55d61637444cd3616a6bfd6cea951a9
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-26 12:22:13 +0800

    * route to login

--- routes/web.php
+++ routes/web.php
@@ -5,14 +5,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
