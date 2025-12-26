190e9d39a4e9339a3b8f773aa7a97eda025199f5
Shashimii <134041347+Shashimii@users.noreply.github.com>
2025-12-26 15:03:43 +0800

    # Migrations

---
+++ database/migrations/2025_12_26_064642_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->nullable();
+            $table->foreignId('employee_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('division_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('last_action')->nullable();
+            $table->string('status')->nullable();
+            $table->string('priority')->nullable();
+            $table->timestamp('due_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
